16294a92499337a6c0333c8729adf9bf90a2e053
tolumorohunfola <111201704+tolumorohunfola@users.noreply.github.com>
2024-02-02 20:02:44 +0000

    Modified testing file to have all of the testing information

--- calculations.py
+++ calculations.py
@@ -1,8 +1,6 @@
 # calculating the net-worth based on just two figures
-import testing
 
 def net_worth_calculation(total_savings,total_debt):
     net_worth = total_savings - total_debt
     print(f"Your net-worth is {net_worth}")
 
-net_worth_calculation(testing.user_savings_total, testing.user_debt_total)
--- testing.py
+++ testing.py
@@ -1,2 +1,5 @@
+from calculations import net_worth_calculation
+
 user_debt_total = 140000
 user_savings_total = 500000
+net_worth_calculation(user_savings_total, user_debt_total)
